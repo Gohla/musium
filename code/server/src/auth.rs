e3d5b7e8156293b33ee5f946266851b4fd5c43a0
Gabriel Konat <gabrielkonat@gmail.com>
2020-03-15 10:21:02 +0100

    Improve auth error handling.

--- code/server/src/auth.rs
+++ code/server/src/auth.rs
@@ -2,31 +2,56 @@ use std::future::Future;
 use std::pin::Pin;
 
 use actix_identity::Identity;
-use actix_web::{FromRequest, HttpRequest, HttpResponse, web};
+use actix_web::{FromRequest, HttpRequest, HttpResponse, ResponseError, web};
 use actix_web::dev::{Payload, PayloadStream};
 use actix_web::error::BlockingError;
+use actix_web::http::StatusCode;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
-use backend::{Backend, BackendConnected};
+use backend::{Backend, BackendConnected, BackendConnectError, UserAddVerifyError};
 use core::model::{User, UserLogin};
-
-use crate::util::ResultExt;
 
 // Handlers
 
-pub async fn login(login_data: web::Json<UserLogin>, identity: Identity, backend: web::Data<Backend>) -> actix_web::Result<HttpResponse> {
-  let user: Result<Option<User>, BlockingError<anyhow::Error>> = web::block(move || {
+#[derive(Debug, Error)]
+pub enum LoginError {
+  #[error(transparent)]
+  BackendConnectFail(#[from] BackendConnectError),
+  #[error(transparent)]
+  UserVerifyFail(#[from] UserAddVerifyError),
+  #[error("Thread pool is gone")]
+  ThreadPoolGoneFail,
+  #[error(transparent)]
+  SerializeIdentityFail(#[from] serde_json::Error),
+}
+
+impl ResponseError for LoginError {}
+
+pub async fn login(login_data: web::Json<UserLogin>, identity: Identity, backend: web::Data<Backend>) -> Result<HttpResponse, LoginError> {
+  use LoginError::*;
+
+  let user: Result<Option<User>, BlockingError<LoginError>> = web::block(move || {
     let backend_connected: BackendConnected = backend.connect_to_database()?;
     Ok(backend_connected.verify_user(&login_data.name, &login_data.password)?)
   }).await;
 
-  if let Some(user) = user? {
-    let logged_in_user = LoggedInUser { user };
-    let serialized_identity = serde_json::to_string(&logged_in_user).map_internal_err()?;
-    identity.remember(serialized_identity);
-    Ok(HttpResponse::Ok().finish())
-  } else {
-    Ok(HttpResponse::Unauthorized().finish())
+  match user {
+    Err(BlockingError::Error(e)) => {
+      Err(e)
+    }
+    Err(BlockingError::Canceled) => {
+      Err(ThreadPoolGoneFail)
+    }
+    Ok(Some(user)) => {
+      let logged_in_user = LoggedInUser { user };
+      let serialized_identity = serde_json::to_string(&logged_in_user)?;
+      identity.remember(serialized_identity);
+      Ok(HttpResponse::Ok().finish())
+    }
+    Ok(None) => {
+      Ok(HttpResponse::Unauthorized().finish())
+    }
   }
 }
 
@@ -42,19 +67,39 @@ pub struct LoggedInUser {
   pub user: User,
 }
 
+#[derive(Debug, Error)]
+pub enum LoggedInUserExtractError {
+  #[error(transparent)]
+  JsonSerDeIdentityFail(#[from] serde_json::Error),
+  #[error(transparent)]
+  IdentityExtractFail(#[from] actix_web::Error),
+  #[error("Not logged in")]
+  NotLoggedInFail,
+}
+
+impl ResponseError for LoggedInUserExtractError {
+  fn status_code(&self) -> StatusCode {
+    match self {
+      LoggedInUserExtractError::NotLoggedInFail => StatusCode::UNAUTHORIZED,
+      _ => StatusCode::INTERNAL_SERVER_ERROR
+    }
+  }
+}
+
 impl FromRequest for LoggedInUser {
-  type Error = actix_web::Error;
-  type Future = Pin<Box<dyn Future<Output=actix_web::Result<LoggedInUser>>>>;
+  type Error = LoggedInUserExtractError;
+  type Future = Pin<Box<dyn Future<Output=Result<LoggedInUser, LoggedInUserExtractError>>>>;
   type Config = ();
 
   fn from_request(req: &HttpRequest, payload: &mut Payload<PayloadStream>) -> Self::Future {
+    use LoggedInUserExtractError::*;
     let identity = Identity::from_request(req, payload);
     Box::pin(async move {
       if let Some(serialized_identity) = identity.await?.identity() {
         let logged_in_user = serde_json::from_str(&serialized_identity)?;
         Ok(logged_in_user)
       } else {
-        Err(actix_web::error::ErrorUnauthorized("Not logged in"))
+        Err(NotLoggedInFail)
       }
     })
   }
