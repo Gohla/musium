5e89ac56ce68e997161acb8e1c1faa2a69483638
Gabriel Konat <gabrielkonat@gmail.com>
2020-08-01 15:13:16 +0200

    Improve error handling in client.

--- musium_client/src/lib.rs
+++ musium_client/src/lib.rs
@@ -1,11 +1,10 @@
-use reqwest::{
-  Client as HttpClient,
-  header::ToStrError,
-  redirect,
-  Response,
-  StatusCode,
-};
+#![feature(backtrace)]
+
+use std::backtrace::Backtrace;
+
+use reqwest::{Client as HttpClient, header::ToStrError, Method, redirect, RequestBuilder, Response, StatusCode};
 pub use reqwest::Url;
+use serde::Serialize;
 use thiserror::Error;
 
 use musium_core::{
@@ -14,7 +13,6 @@ use musium_core::{
     *,
     collection::{Albums, AlbumsRaw, Tracks, TracksRaw},
   },
-  untagged_result::UResult,
 };
 
 pub struct Client {
@@ -40,107 +38,94 @@ impl Client {
   }
 }
 
-// Generic error type for client failures
+// Generic HTTP request error type for the most common errors
 
 #[derive(Debug, Error)]
-pub enum ClientError {
+pub enum HttpRequestError {
   #[error(transparent)]
   UrlJoinFail(#[from] url::ParseError),
   #[error(transparent)]
-  HttpRequestFail(#[from] reqwest::Error),
-  #[error(transparent)]
-  InternalServerFail(#[from] InternalServerError),
-  #[error(transparent)]
-  HeaderValueToStringFail(#[from] ToStrError),
-  #[error("Unexpected response {0:?} from the server")]
-  UnexpectedResponseFail(StatusCode),
+  RequestFail(#[from] reqwest::Error),
+  #[error("Server responded with an internal error")]
+  InternalServerFail(#[from] InternalServerError, Backtrace),
+  #[error("Server responded with unexpected status code {0}")]
+  UnexpectedStatusCode(StatusCode, Backtrace),
 }
 
 // Login
 
 impl Client {
-  pub async fn login(&self, user_login: &UserLogin) -> Result<User, ClientError> {
-    let url = self.url.join("login")?;
-    let user = self.client.post(url)
-      .json(user_login)
-      .send().await?
-      .json().await?;
-    Ok(user)
+  pub async fn login(&self, user_login: &UserLogin) -> Result<User, HttpRequestError> {
+    let response = self.post_simple_with_json("login", user_login).await?;
+    Ok(response.json().await?)
   }
 }
 
 // Local source
 
 impl Client {
-  pub async fn list_local_sources(&self) -> Result<Vec<LocalSource>, ClientError> {
-    let local_sources: UResult<_, InternalServerError> = self.client.get(self.url.join("source/local")?)
-      .send().await?
-      .json().await?;
-    Ok(local_sources?)
+  pub async fn list_local_sources(&self) -> Result<Vec<LocalSource>, HttpRequestError> {
+    let response = self.get_simple("source/localz").await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn get_local_source_by_id(&self, id: i32) -> Result<Option<LocalSource>, ClientError> {
-    let local_source = self.client.get(self.url.join(&format!("source/local/{}", id))?)
-      .send().await?
-      .json().await?;
-    Ok(local_source)
+  pub async fn get_local_source_by_id(&self, id: i32) -> Result<Option<LocalSource>, HttpRequestError> {
+    let response = self.get_simple(format!("source/local/{}", id)).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn create_or_enable_local_source(&self, new_local_source: &NewLocalSource) -> Result<LocalSource, ClientError> {
-    let local_source = self.client.post(self.url.join("source/local")?)
-      .json(new_local_source)
-      .send().await?
-      .json().await?;
-    Ok(local_source)
+  pub async fn create_or_enable_local_source(&self, new_local_source: &NewLocalSource) -> Result<LocalSource, HttpRequestError> {
+    let response = self.post_simple_with_json("source/local", new_local_source).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn set_local_source_enabled_by_id(&self, id: i32, enabled: bool) -> Result<Option<LocalSource>, ClientError> {
-    let local_source = self.client.post(self.url.join(&format!("source/local/set_enabled/{}", id))?)
-      .json(&enabled)
-      .send().await?
-      .json().await?;
-    Ok(local_source)
+  pub async fn set_local_source_enabled_by_id(&self, id: i32, enabled: bool) -> Result<Option<LocalSource>, HttpRequestError> {
+    let response = self.post_simple_with_json(format!("source/local/set_enabled/{}", id), &enabled).await?;
+    Ok(response.json().await?)
   }
 }
 
 // Spotify source
 
+#[derive(Debug, Error)]
+pub enum CreateSpotifySourceAuthorizationUrlError {
+  #[error(transparent)]
+  HttpRequestFail(#[from] HttpRequestError),
+  #[error("The LOCATION header is missing from the HTTP response")]
+  LocationHeaderMissingFail,
+  #[error("Failed to convert the LOCATION header in the HTTP response to a string")]
+  LocationHeaderToStringFail(#[from] ToStrError),
+}
+
 impl Client {
-  pub async fn create_spotify_source_authorization_url(&self) -> Result<String, ClientError> {
-    use ClientError::*;
-    let response = self.client.get(self.url.join("source/spotify/request_authorization")?)
-      .send().await?
-      .error_for_status()?;
+  pub async fn create_spotify_source_authorization_url(&self) -> Result<String, CreateSpotifySourceAuthorizationUrlError> {
+    use CreateSpotifySourceAuthorizationUrlError::*;
+    let response = self.get_simple("source/spotify/request_authorization").await?;
     if let Some(url) = response.headers().get(reqwest::header::LOCATION) {
       Ok(url.to_str()?.to_owned())
     } else {
-      Err(UnexpectedResponseFail(response.status()))
+      Err(LocationHeaderMissingFail)
     }
   }
 
-  pub async fn show_spotify_me(&self) -> Result<SpotifyMeInfo, ClientError> {
-    let me_info: SpotifyMeInfo = self.client.get(self.url.join("source/spotify/me")?)
-      .send().await?
-      .json().await?;
-    Ok(me_info)
+  pub async fn show_spotify_me(&self) -> Result<SpotifyMeInfo, HttpRequestError> {
+    let response = self.get_simple("source/spotify/me").await?;
+    Ok(response.json().await?)
   }
 }
 
 // Album
 
 impl Client {
-  pub async fn list_albums(&self) -> Result<Albums, ClientError> {
-    let albums_raw: AlbumsRaw = self.client.get(self.url.join("album")?)
-      .send().await?
-      .json().await?;
+  pub async fn list_albums(&self) -> Result<Albums, HttpRequestError> {
+    let response = self.get_simple("album").await?;
+    let albums_raw: AlbumsRaw = response.json().await?;
     Ok(albums_raw.into())
   }
 
-  pub async fn get_album_by_id(&self, id: i32) -> Result<Option<LocalAlbum>, ClientError> {
-    let local_album = self.client.get(self.url.join(&format!("album/{}", id))?)
-      .send().await?
-      .json().await?;
-    Ok(local_album)
+  pub async fn get_album_by_id(&self, id: i32) -> Result<Option<LocalAlbum>, HttpRequestError> {
+    let response = self.get_simple(format!("album/{}", id)).await?;
+    Ok(response.json().await?)
   }
 }
 
@@ -152,28 +137,28 @@ pub enum PlaySource {
 }
 
 impl Client {
-  pub async fn list_tracks(&self) -> Result<Tracks, ClientError> {
-    let tracks_raw: TracksRaw = self.client.get(self.url.join("track")?)
-      .send().await?
-      .json().await?;
+  pub async fn list_tracks(&self) -> Result<Tracks, HttpRequestError> {
+    let response = self.get_simple("album").await?;
+    let tracks_raw: TracksRaw = response.json().await?;
     Ok(tracks_raw.into())
   }
 
-  pub async fn get_track_by_id(&self, id: i32) -> Result<Option<LocalTrack>, ClientError> {
-    let track = self.client.get(self.url.join(&format!("track/{}", id))?)
-      .send().await?
-      .json().await?;
-    Ok(track)
+  pub async fn get_track_by_id(&self, id: i32) -> Result<Option<LocalTrack>, HttpRequestError> {
+    let response = self.get_simple(format!("track/{}", id)).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn play_track_by_id(&self, id: i32) -> Result<Option<PlaySource>, ClientError> {
-    let response = self.client.get(self.url.join(&format!("track/play/{}", id))?)
-      .send().await?;
+  pub async fn play_track_by_id(&self, id: i32) -> Result<Option<PlaySource>, HttpRequestError> {
+    let response = self.get(
+      format!("track/play/{}", id),
+      |r| r,
+      &[StatusCode::OK, StatusCode::ACCEPTED, StatusCode::NOT_FOUND],
+    ).await?;
     let play_source = match response.status() {
       StatusCode::OK => Some(PlaySource::AudioData(response.bytes().await?.to_vec())),
       StatusCode::ACCEPTED => Some(PlaySource::ExternallyPlayed),
       StatusCode::NOT_FOUND => None,
-      c => return Err(ClientError::UnexpectedResponseFail(c)),
+      _ => unreachable!()
     };
     Ok(play_source)
   }
@@ -182,65 +167,47 @@ impl Client {
 // Artist
 
 impl Client {
-  pub async fn list_artists(&self) -> Result<Vec<LocalArtist>, ClientError> {
-    let scan_directories = self.client.get(self.url.join("artist")?)
-      .send().await?
-      .json().await?;
-    Ok(scan_directories)
+  pub async fn list_artists(&self) -> Result<Vec<LocalArtist>, HttpRequestError> {
+    let response = self.get_simple("artist").await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn get_artist_by_id(&self, id: i32) -> Result<Option<LocalArtist>, ClientError> {
-    let artist = self.client.get(self.url.join(&format!("artist/{}", id))?)
-      .send().await?
-      .json().await?;
-    Ok(artist)
+  pub async fn get_artist_by_id(&self, id: i32) -> Result<Option<LocalArtist>, HttpRequestError> {
+    let response = self.get_simple(format!("artist/{}", id)).await?;
+    Ok(response.json().await?)
   }
 }
 
 // User
 
 impl Client {
-  pub async fn list_users(&self) -> Result<Vec<User>, ClientError> {
-    let users = self.client.get(self.url.join("user")?)
-      .send().await?
-      .json().await?;
-    Ok(users)
+  pub async fn list_users(&self) -> Result<Vec<User>, HttpRequestError> {
+    let response = self.get_simple("user").await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn get_my_user(&self) -> Result<User, ClientError> {
-    let user = self.client.get(self.url.join("user/me")?)
-      .send().await?
-      .json().await?;
-    Ok(user)
+  pub async fn get_my_user(&self) -> Result<User, HttpRequestError> {
+    let response = self.get_simple("user/me").await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn get_user_by_id(&self, id: i32) -> Result<Option<User>, ClientError> {
-    let user = self.client.get(self.url.join(&format!("user/{}", id))?)
-      .send().await?
-      .json().await?;
-    Ok(user)
+  pub async fn get_user_by_id(&self, id: i32) -> Result<Option<User>, HttpRequestError> {
+    let response = self.get_simple(format!("user/{}", id)).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn create_user(&self, new_user: &NewUser) -> Result<User, ClientError> {
-    let user = self.client.post(self.url.join("user")?)
-      .json(new_user)
-      .send().await?
-      .json().await?;
-    Ok(user)
+  pub async fn create_user(&self, new_user: &NewUser) -> Result<User, HttpRequestError> {
+    let response = self.post_simple_with_json("user", new_user).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn delete_user_by_name(&self, name: &String) -> Result<(), ClientError> {
-    self.client.delete(self.url.join("user")?)
-      .json(name)
-      .send().await?
-      .error_for_status()?;
+  pub async fn delete_user_by_name(&self, name: &String) -> Result<(), HttpRequestError> {
+    self.delete_simple_with_json("user", name).await?;
     Ok(())
   }
 
-  pub async fn delete_user_by_id(&self, id: i32) -> Result<(), ClientError> {
-    self.client.delete(self.url.join(&format!("user/{}", id))?)
-      .send().await?
-      .error_for_status()?;
+  pub async fn delete_user_by_id(&self, id: i32) -> Result<(), HttpRequestError> {
+    self.delete_simple(format!("user/{}", id)).await?;
     Ok(())
   }
 }
@@ -248,39 +215,183 @@ impl Client {
 // User data
 
 impl Client {
-  pub async fn set_user_album_rating(&self, album_id: i32, rating: i32) -> Result<UserAlbumRating, ClientError> {
-    let rating = self.client.put(self.url.join(&format!("user/data/album/{}/rating/{}", album_id, rating))?)
-      .send().await?
-      .json().await?;
-    Ok(rating)
+  pub async fn set_user_album_rating(&self, album_id: i32, rating: i32) -> Result<UserAlbumRating, HttpRequestError> {
+    let response = self.put_simple(format!("user/data/album/{}/rating/{}", album_id, rating)).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn set_user_track_rating(&self, track_id: i32, rating: i32) -> Result<UserTrackRating, ClientError> {
-    let rating = self.client.put(self.url.join(&format!("user/data/album/{}/rating/{}", track_id, rating))?)
-      .send().await?
-      .json().await?;
-    Ok(rating)
+  pub async fn set_user_track_rating(&self, track_id: i32, rating: i32) -> Result<UserTrackRating, HttpRequestError> {
+    let response = self.put_simple(format!("user/data/track/{}/rating/{}", track_id, rating)).await?;
+    Ok(response.json().await?)
   }
 
-  pub async fn set_user_artist_rating(&self, artist_id: i32, rating: i32) -> Result<UserArtistRating, ClientError> {
-    let rating = self.client.put(self.url.join(&format!("user/data/album/{}/rating/{}", artist_id, rating))?)
-      .send().await?
-      .json().await?;
-    Ok(rating)
+  pub async fn set_user_artist_rating(&self, artist_id: i32, rating: i32) -> Result<UserArtistRating, HttpRequestError> {
+    let response = self.put_simple(format!("user/data/artist/{}/rating/{}", artist_id, rating)).await?;
+    Ok(response.json().await?)
   }
 }
 
 // Sync
 
 impl Client {
-  pub async fn sync(&self) -> Result<bool, ClientError> {
-    use ClientError::*;
-    let response: Response = self.client.get(self.url.join("sync")?)
-      .send().await?;
+  pub async fn sync(&self) -> Result<bool, HttpRequestError> {
+    let response = self.get(
+      "sync",
+      |r| r,
+      &[StatusCode::OK, StatusCode::ACCEPTED],
+    ).await?;
     match response.status() {
-      StatusCode::ACCEPTED => Ok(true),
       StatusCode::OK => Ok(false),
-      c => Err(UnexpectedResponseFail(c))
+      StatusCode::ACCEPTED => Ok(true),
+      _ => unreachable!(),
     }
+  }
+}
+
+// Internals
+
+#[allow(dead_code)]
+impl Client {
+  async fn request(
+    &self,
+    method: Method,
+    url_suffix: impl AsRef<str>,
+    f_request: impl FnOnce(RequestBuilder) -> RequestBuilder,
+    expected_status_codes: impl AsRef<[StatusCode]>,
+  ) -> Result<Response, HttpRequestError> {
+    use HttpRequestError::*;
+    let url = self.url.join(url_suffix.as_ref())?;
+    let response = f_request(self.client.request(method, url)).send().await?;
+    match response.status() {
+      c @ StatusCode::INTERNAL_SERVER_ERROR => {
+        let json: Result<InternalServerError, _> = response.json().await;
+        return Err(if let Ok(internal_server_error) = json {
+          InternalServerFail(internal_server_error, Backtrace::capture())
+        } else {
+          UnexpectedStatusCode(c, Backtrace::capture())
+        });
+      }
+      c if !expected_status_codes.as_ref().contains(&c) => {
+        return Err(UnexpectedStatusCode(c, Backtrace::capture()));
+      }
+      _ => {}
+    }
+    Ok(response)
+  }
+
+  async fn request_simple(
+    &self,
+    method: Method,
+    url_suffix: impl AsRef<str>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request(method, url_suffix, |r| r, &[StatusCode::OK]).await
+  }
+
+  async fn request_simple_with_json(
+    &self,
+    method: Method,
+    url_suffix: impl AsRef<str>,
+    json: &(impl Serialize + ?Sized),
+  ) -> Result<Response, HttpRequestError> {
+    self.request(method, url_suffix, |r| r.json(json), &[StatusCode::OK]).await
+  }
+
+  async fn get(
+    &self,
+    url_suffix: impl AsRef<str>,
+    f_request: impl FnOnce(RequestBuilder) -> RequestBuilder,
+    expected_status_codes: impl AsRef<[StatusCode]>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request(Method::GET, url_suffix, f_request, expected_status_codes).await
+  }
+
+  async fn get_simple(
+    &self,
+    url_suffix: impl AsRef<str>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple(Method::GET, url_suffix).await
+  }
+
+  async fn get_simple_with_json(
+    &self,
+    url_suffix: impl AsRef<str>,
+    json: &(impl Serialize + ?Sized),
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple_with_json(Method::GET, url_suffix, json).await
+  }
+
+
+  async fn post(
+    &self,
+    url_suffix: impl AsRef<str>,
+    f_request: impl FnOnce(RequestBuilder) -> RequestBuilder,
+    expected_status_codes: impl AsRef<[StatusCode]>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request(Method::POST, url_suffix, f_request, expected_status_codes).await
+  }
+
+  async fn post_simple(
+    &self,
+    url_suffix: impl AsRef<str>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple(Method::POST, url_suffix).await
+  }
+
+  async fn post_simple_with_json(
+    &self,
+    url_suffix: impl AsRef<str>,
+    json: &(impl Serialize + ?Sized),
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple_with_json(Method::POST, url_suffix, json).await
+  }
+
+
+  async fn put(
+    &self,
+    url_suffix: impl AsRef<str>,
+    f_request: impl FnOnce(RequestBuilder) -> RequestBuilder,
+    expected_status_codes: impl AsRef<[StatusCode]>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request(Method::PUT, url_suffix, f_request, expected_status_codes).await
+  }
+
+  async fn put_simple(
+    &self,
+    url_suffix: impl AsRef<str>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple(Method::PUT, url_suffix).await
+  }
+
+  async fn put_simple_with_json(
+    &self,
+    url_suffix: impl AsRef<str>,
+    json: &(impl Serialize + ?Sized),
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple_with_json(Method::PUT, url_suffix, json).await
+  }
+
+
+  async fn delete(
+    &self,
+    url_suffix: impl AsRef<str>,
+    f_request: impl FnOnce(RequestBuilder) -> RequestBuilder,
+    expected_status_codes: impl AsRef<[StatusCode]>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request(Method::DELETE, url_suffix, f_request, expected_status_codes).await
+  }
+
+  async fn delete_simple(
+    &self,
+    url_suffix: impl AsRef<str>,
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple(Method::DELETE, url_suffix).await
+  }
+
+  async fn delete_simple_with_json(
+    &self,
+    url_suffix: impl AsRef<str>,
+    json: &(impl Serialize + ?Sized),
+  ) -> Result<Response, HttpRequestError> {
+    self.request_simple_with_json(Method::DELETE, url_suffix, json).await
   }
 }
